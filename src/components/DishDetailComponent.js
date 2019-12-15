c8f64e2719f37c70ddc20bc9f4ce104ff30ae5a8
Zeshan <murtza01@ads.uni-passau.de>
2019-12-15 11:23:11 +0100

    Functional components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component {
 
-    renderComments(comments) {
+    function RenderComments(comments) {
         if (comments == null) {
             return (<div></div>)
         }
@@ -33,7 +32,7 @@ class Dishdetail extends Component {
         )
     }
 
-    renderDish(dish) {
+    function RenderDish(dish) {
         if (dish != null) {
             return (
                 <div>
@@ -52,13 +51,13 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        const dish = this.props.dish
+    const Dishdetail = (props) => {
+        const dish = props.dish
         if (dish == null) {
             return (<div></div>)
         }
-        const dishItem = this.renderDish(dish)
-        const commentItem = this.renderComments(dish.comments)
+        const dishItem =RenderDish(dish)
+        const commentItem =RenderComments(dish.comments)
         return (
             <div className = "container">
                 <div className='row'>
@@ -74,6 +73,8 @@ class Dishdetail extends Component {
             
         )
     }
-}
 
-export default Dishdetail
+export default Dishdetail;
+
+//If we are not using any constructor or local states in a component, then we have an option to remove class name extends component
+// and render function. Instead of this we can use user define functions and use props of main class while getting properties from parent main class
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
-import DishDetail from './DishDetailComponent'
-import Main from './MainComponent'
 
-
-class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-
-    }
-
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.props.onClick(dish.id)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
-
+    // instead of giving these two parameters in the form of objects, We can also use props 
+    // just like props used in the second function
+    // for user defined components, we use capital letter for writing name of function
+    function RenderMenueItem({dish, onClick}) {
         return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
-        );
-    }
-}
+            <Card 
+            onClick={() =>onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
 
+        )
+    }
+
+    function Menu(props) {
+            const menu = props.dishes.map((dish) => {
+                return (
+                    <div  className="col-12 col-md-5 m-1" key={dish.id}>
+                        <RenderMenueItem dish = {dish} onClick = {props.onClick}/>
+                    </div>
+                );
+            });
+        
+            return (
+                <div className="container">
+                    <div className="row">
+                        {menu}
+                    </div>
+                </div>
+            );
+        
+    }
+
+   
 
 export default Menu;
+
+//If we are not using any constructor or local states in a component, then we have an option to remove class name extends component
+// and render function. Instead of this we can use user define functions and use props of main class while getting properties from parent main class
