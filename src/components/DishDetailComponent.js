467b64f4829d7827d0a11cc4080e685cc6a521bf
Zeshan <murtza01@ads.uni-passau.de>
2019-12-28 12:39:06 +0100

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem,Modal, ModalBody, ModalHeader, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form'
+import { Loading } from "./LoadingComponent";
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -134,7 +135,25 @@ class CommentForm extends Component{
     }
 
     const Dishdetail = (props) => {
-        if (props.dish!=null) {
+        if (props.isLoading){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            )
+        }
+        else if(props.errMess){
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            )
+        }
+        else if (props.dish!=null) {
             return (
                 <div className = "container">
                     <div className="row">
